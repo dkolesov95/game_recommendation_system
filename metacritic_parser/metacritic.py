89e2d5903114e90a490b7b5bd29a83499f3a9670
dkolesov95 <dmitriykolesov95@gmail.com>
2022-11-12 21:54:21 +0200

    add get_data_from_page and collect_game_data

--- metacritic_parser/metacritic.py
+++ metacritic_parser/metacritic.py
@@ -1,25 +1,44 @@
-from typing import Union
+from typing import Union, Tuple
 
 from requests import Response
 from requests_html import HTMLSession
 
 
 class Parser:
-    _BASE_URL = 'https://www.metacritic.com'
-
     def __init__(self, year):
         self.year = year
         self.session = HTMLSession()
 
-    def get_pagination_count_pages(self) -> Union[tuple[Response, None], tuple[Response, int]]:
-        url = self._BASE_URL + f'/browse/games/score/metascore/year/all/filtered?year_selected={self.year}&distribution=&sort=desc&view=detailed&page=0'
+    def get_data_from_page(self, pagination_num) -> Union[Tuple[Response, list[tuple]], Tuple[None, None]]:
+        url = f'https://www.metacritic.com/browse/games/score/metascore/year/all/filtered?year_selected={self.year}&distribution=&sort=desc&view=detailed&page={pagination_num}'
         request = self.session.get(url)
 
         if request.status_code != 200:
             print(f'get_pagination_count_pages error, status code {request.status_code}')
-            return request, None
+            return None, None
 
-        last_page = request.html.find('.last_page', first=True)
-        pagi_num = int(last_page.find('.page_num', first=True).text) if last_page else None
+        games_data = []
+        list_wrapper = request.html.find('.browse_list_wrapper')
+        for lw in list_wrapper:
+            trs = lw.find('tr')
+            for tr in trs:
+                if not tr.text:
+                    continue
+                games_data.append(self.collect_game_data(tr))
+        return request, games_data
 
-        return request, pagi_num
+    @staticmethod
+    def collect_game_data(game_card: Response) -> tuple:
+        title = game_card.find('h3', first=True).text
+        platform = game_card.find('.platform', first=True).find('.data', first=True).text
+        date = game_card.find('.clamp-details', first=True).find('span')[-1].text
+        summary = game_card.find('.summary', first=True).text
+        metascore = game_card.find('.clamp-metascore', first=True).find('.metascore_w', first=True).text
+        userscore = game_card.find('.clamp-userscore', first=True).find('.metascore_w', first=True).text
+        href = game_card.find('a', first=True).attrs['href']
+        return title, platform, date, summary, metascore, userscore, href
+
+    # @staticmethod
+    # def get_pagination_count_pages(request: Response) -> Union[int, None]:
+    #     last_page = request.html.find('.last_page', first=True)
+    #     return int(last_page.find('.page_num', first=True).text) if last_page else None
